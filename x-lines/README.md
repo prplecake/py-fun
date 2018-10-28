b8a75cb87899d3852f599242aaaf0c806562603d
Matthew Jorgensen <matthew@jrgnsn.net>
2018-10-27 20:01:38 -0500

    Add x-lines
    x-lines was a solution to a problem that hardly existed.

---
+++ x-lines/README.md
@@ -0,0 +1,40 @@
+x-lines
+=======
+
+So you want to keep track of how you set up a new system, but you *know* the `.*sh_history` file is already thousands of lines long. This script here will only copy the first x lines (you specify) from any file (you specify). Then, (***not implemented yet***) the script will save the lines to a new files called something like `*sh_history.<time_stamp>`.
+
+_Where \* = your shell (bash, zsh, etc.)_ and `<time_stamp>` is in the format of `%Y-%m-%d-%H-%M-%S`.
+
+Easy enough, right?
+
+## Usage
+It's pretty straightforward.
+
+``` shell
+$ python copy_x_files.py -h
+usage: copy_x_lines.py [-h] [-d] filename lines
+
+positional arguments:
+  filename     the file to be copied from
+  lines        the number of lines to copy from the beginning of the file
+
+optional arguments:
+  -h, --help   show this help message and exit
+  -d, --debug  enables some (sparse) debugging features
+```
+
+So...
+
+``` shell
+$ python copy_x_files.py ~/.zsh_history 50
+```
+
+...will copy the first 50 lines of `~/.zsh_history` to `~/zsh_history_<timestamp>`.
+
+And...
+
+``` shell
+$ python copy_x_files.py ~/example.txt 100
+```
+
+...will copy the first 100 lines of `~/example.txt` to `~/example_<timestamp>.txt`.
---
+++ x-lines/copy_x_lines.py
@@ -0,0 +1,86 @@
+## copy_x_lines.py
+## 2016-02-15
+## https://github.com/mattjorgs/x-lines
+##
+## Author:	Matthew Jorgensen
+## Contact:	mattjorgs@gmail.com
+## Version:	1.0
+## Latest Revision:	2016-02-19
+##
+## This script copies x-number of lines from a file and copies them to a new
+## file with a timestamp appended. If it's a dotfile, the leading dot is
+## stripped.
+##
+## To do:
+## * Better naming with files with extensions (see issue #5)
+
+import os, argparse, datetime
+
+# initialize the argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", help="the file to be copied from")
+parser.add_argument("lines", type=int,
+	help="the number of lines to copy from the beginning of the file")
+parser.add_argument("-d", "--debug", action="store_true",
+	help="enables some (sparse) debugging features")
+args = parser.parse_args()
+
+if args.debug:
+	debug = True
+else:
+	debug = False
+
+# remove leading dot from dotfiles:
+def RemoveLeadingDot(filename):
+	head, tail = os.path.split(filename)
+	if tail[:1] == ".":
+		return os.path.join(head, tail[1:])
+	else:
+		return filename
+
+if debug == True:
+	print "Leading dot test: %s" % RemoveLeadingDot(args.filename)
+
+# append timestamp to filename
+def StampTime(filename, fmt='{fname}_%Y-%m-%d-%H-%M-%S{fext}'):
+	# disect filename
+	head, tail = os.path.split(filename)
+	# disect tail ("test.txt" -> "text", ".txt")
+	name, extension = os.path.splitext(tail)
+	sansExt = os.path.join(head, name)
+	return datetime.datetime.now().strftime(fmt).format(fname=sansExt,fext=extension)
+
+datedFilename = StampTime(RemoveLeadingDot(args.filename))
+
+if debug == True:
+	print "datedFilename: %s" % datedFilename
+
+def GetNumLines(filename):
+	with open(filename) as target:
+		num_lines = sum(1 for line in target)
+	return num_lines
+
+if debug == True:
+	print "Number of lines in file: %d" % GetNumLines(args.filename)
+
+def CheckLines(linesWanted, linesInFile):
+	if linesWanted <= linesInFile:
+		pass
+	elif linesWanted > linesInFile:
+		print "You're asking for too many lines."
+		print "The file only has %d lines." % linesInFile
+		exit()
+
+def CopyFile(readFile, writeFile, lines):
+	with open(writeFile, "w") as wtarget:
+		with open(readFile, "r") as rtarget:
+			i = 0
+			while i <= lines + 1:
+				wtarget.write(rtarget.readline(i))
+				i+=1
+if __name__ == '__main__':
+	if debug == True:
+		pass
+	else:
+		CheckLines(args.lines, GetNumLines(args.filename))
+		CopyFile(args.filename, datedFilename, args.lines)
