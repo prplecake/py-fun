8686cd7a4c7db4182c2be83e7157075fdfd5b3b5
Matthew Jorgensen <matthew@jrgnsn.net>
2019-01-22 00:31:57 -0600

    Linted and possibly made Python 3 compatible.

--- x-lines/write_x_lines.py
+++ x-lines/write_x_lines.py
@@ -1,9 +1,9 @@
 # file_open = open("file_to_open.txt", "r")  # (file, permissions)
 # file_output = open("output.txt", "w")
-#
+
 # for line in file_open:
-#	print("%s"%(line), file=file_output)
-#
+#     print("%s"%(line), file=file_output)
+
 # file_open.(close)
 # file_output.close()
 
@@ -12,32 +12,32 @@ import os
 
 script, filename = argv
 
-print "We're going to erase %r." % filename
-print "If you don't want that, hit CTRL-C."
-print "If you do want that, hit RETURN."
+print("We're going to erase %r." % filename)
+print("If you don't want that, hit CTRL-C.")
+print("If you do want that, hit RETURN.")
 
-raw_input("?")
+input("?")
 
-print "Opening the file..."
+print("Opening the file...")
 target = open(filename, "w")
 
-print "Truncating the file. Au revoir!"
+print("Truncating the file. Au revoir!")
 target.truncate()
 
-print "Now I'm going to print lines."
-lines = int(raw_input("How many lines? "))
+print("Now I'm going to print lines.")
+lines = int(input("How many lines? "))
 
-print "I'll print %d lines." % lines
+print("I'll print %d lines." % lines)
 
 i = 1
 while i <= lines:
-	print "Writing line %d..." % i
-	target.write("%d"%i)
-	target.write("\n")
-	i+=1
+    print("Writing line %d..." % i)
+    target.write("%d" % i)
+    target.write("\n")
+    i += 1
 
-print "Closing the file."
+print("Closing the file.")
 target.close()
 
 size = os.path.getsize(filename)
-print "The file is %d bytes." % size
+print("The file is %d bytes." % size)
